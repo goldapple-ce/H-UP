b7705735540cfacd4d9c39b0c91572aff1f22887
김한중 <hjhzkc@gmail.com>
2024-04-25 18:00:10 +0900

    feat: KanbanBoard 구현

---
+++ frontend/src/components/Kanban/Kanban.jsx
@@ -0,0 +1,45 @@
+import { useRecoilValue } from 'recoil';
+import KanbanList from '../Kanban/KanbanList';
+import Card from '../Kanban/Card';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { kanbanListState } from '../Kanban/recoil';
+import styles from './Kanban.module.scss'
+
+function Kanban() {
+  const kanbanList = useRecoilValue(kanbanListState);
+  const { TO_DO, IN_PROGRESS, DONE, NOTE } = TITLE_NAME;
+
+  const cardDataHandler = (cardTitle) => {
+    return kanbanList
+      .filter((data) => data.category === cardTitle)
+      .map((item, index) => <Card key={item.id} item={item} />);
+  };
+
+  return (
+    <div>
+      <header>
+        <span className="title">칸반 보드!!!</span>
+      </header>
+      <section className={styles.kanbanListContainer}>
+        <DndProvider backend={HTML5Backend}>
+          <KanbanList title={TO_DO}>{cardDataHandler(TO_DO)}</KanbanList>
+          <KanbanList title={IN_PROGRESS}>
+            {cardDataHandler(IN_PROGRESS)}
+          </KanbanList>
+          <KanbanList title={DONE}>{cardDataHandler(DONE)}</KanbanList>
+          <KanbanList title={NOTE}>{cardDataHandler(NOTE)}</KanbanList>
+        </DndProvider>
+      </section>
+    </div>
+  );
+}
+
+export default Kanban;
+
+export const TITLE_NAME = {
+  TO_DO: 'To do',
+  IN_PROGRESS: 'In progress',
+  DONE: 'Done',
+  NOTE: 'Notes & Reference',
+};
---
+++ frontend/src/components/Kanban/KanbanCreator.jsx
@@ -0,0 +1,38 @@
+import { useCallback } from 'react';
+import { useRecoilState } from 'recoil';
+import { kanbanListState } from './recoil';
+import React from 'react';
+import './KanbanCreator.module.scss';
+
+function KanbanCreator({ title }) {
+  const [kanbanList, setKanbanList] = useRecoilState(kanbanListState);
+
+  const getId =
+    kanbanList.length > 0 ? kanbanList[kanbanList.length - 1].id + 1 : 0;
+
+  const addCard = useCallback(
+    (e) => {
+      setKanbanList((prev) => [
+        ...prev,
+        {
+          id: getId,
+          title: '',
+          content: '',
+          category: title,
+          isChecked: false,
+        },
+      ]);
+    },
+    [getId, setKanbanList, title]
+  );
+
+  return (
+    <div className="addBtnWrap">
+      <button className="cardAddBtn" onClick={addCard}>
+        + Add task
+      </button>
+    </div>
+  );
+}
+
+export default React.memo(KanbanCreator);
