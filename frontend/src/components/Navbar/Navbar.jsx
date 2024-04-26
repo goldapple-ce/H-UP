e1f2909b4e698713909cc399b4ce05d65fc2b7d1
김한중 <hjhzkc@gmail.com>
2024-04-26 16:35:47 +0900

    feat: Navbar 사이드바 버튼 구현

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,23 @@
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.module.scss'
+import { useRecoilState } from "recoil";
+import { MenuSidebarState, MessengerSidebarState } from '../../recoil/recoil';
+
 
 const NavBar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useRecoilState(MenuSidebarState);
+
+  const ShowMenuSidebar = () => {
+      isMenuOpen === true ? setIsMenuOpen(false) : setIsMenuOpen(true);
+  }
+
+  const [isMessengerOpen, setIsMessengerOpen] = useRecoilState(MessengerSidebarState);
+
+  const ShowMessengerSidebar = () => {
+      isMessengerOpen === true ? setIsMessengerOpen(false) : setIsMessengerOpen(true);
+  }
+
 
   return (
     <nav>
@@ -11,6 +26,8 @@ const NavBar = () => {
         <li><Link to="/" className="navbar-brand text-secondary mr-0">Home</Link></li>
         <li><Link to="/ProjectPage" className="navbar-brand text-secondary mr-0">Project</Link></li>
         <li><Link to="/IssuePage"className="navbar-brand text-secondary mr-0">Issue</Link></li>
+        <li><div className="btn" onClick={ShowMenuSidebar}>메뉴</div></li>
+        <li><div className="btn" onClick={ShowMessengerSidebar}>메신저</div></li>
       </ul>
     </nav>
   );
