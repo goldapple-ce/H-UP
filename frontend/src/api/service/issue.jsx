1edc39c91db8909f1cb1bc5ca932e15887ac6fc0
bluebear9508@naver.com <bluebear9508@naver.com>
2024-04-29 15:58:12 +0900

    trial to make login Logic

--- frontend/src/api/service/issue.jsx
+++ frontend/src/api/service/issue.jsx
@@ -2,5 +2,6 @@ import { api } from "../instance/api";
 
 export const loginAPI = (userId, password) => {
     // axios.post(`${window.location.origin}/api/member/login`, { userId, password });
-    api.post(`api/member/login`, { userId, password });
+    const response = api.post(`api/member/login`, { userId, password });
+    return response;
 }
