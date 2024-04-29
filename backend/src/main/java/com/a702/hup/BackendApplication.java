2de88ca94249703b4e7a665610ed8fcd645c3cba
lkt9899 <lkt9899@naver.com>
2024-04-29 16:57:22 +0900

    refactor : repositories base package scan 추가

--- backend/src/main/java/com/a702/hup/BackendApplication.java
+++ backend/src/main/java/com/a702/hup/BackendApplication.java
@@ -2,8 +2,23 @@ package com.a702.hup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
+@EnableRedisRepositories(basePackages = {
+		"**.redis.**"
+})
+@EnableMongoRepositories(basePackages = {
+		"**.mongodb.**"
+})
+@EnableJpaRepositories(excludeFilters = {
+		@ComponentScan.Filter(type = FilterType.REGEX, pattern = "com.*.redis.*"),
+		@ComponentScan.Filter(type = FilterType.REGEX, pattern = "com.*.mongodb.*")
+})
 public class BackendApplication {
 
 	public static void main(String[] args) {
