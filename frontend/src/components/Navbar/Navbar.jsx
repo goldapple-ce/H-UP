87a923c54dc6ee571d82a7567209b69e39e25a53
bluebear9508@naver.com <bluebear9508@naver.com>
2024-05-01 17:19:59 +0900

    Fix: Trial to fix Navbar Build Error

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { logout } from '../../features/auth/authThunks'
+import { logout } from '../../features/auth/authSlice'
 import './Navbar.module.scss'
 import { useRecoilState } from "recoil";
 import { MenuSidebarState, MessengerSidebarState } from '../../recoil/recoil';
