f98fca8216802246ff9e70039bb68a6a3f24a7a6
김한중 <hjhzkc@gmail.com>
2024-05-02 16:09:28 +0900

    style: Kanban 스타일 변경

--- frontend/src/components/Kanban/Card.jsx
+++ frontend/src/components/Kanban/Card.jsx
@@ -3,12 +3,12 @@ import { useDrag } from 'react-dnd';
 import { useRecoilState } from 'recoil';
 import { TITLE_NAME } from './Kanban';
 import { issueListState } from '../../recoil/recoil';
-import './Card.scss';
 import { useNavigate } from 'react-router-dom';
+import styles from "./Card.module.scss"
 
 function Card({ item }) {
   const [list, setList] = useRecoilState(issueListState);
-  const [badgeColor, setBadgeColor] = useState('');
+  const [cardColor, setCardColor] = useState('');
   const index = list.findIndex((data) => data === item);
   const ref = useRef(null);
   const { TO_DO, IN_PROGRESS, DONE, NOTE } = TITLE_NAME;
@@ -88,16 +88,16 @@ function Card({ item }) {
   useEffect(() => {
     switch (item.category) {
       case TO_DO:
-        setBadgeColor('#ef5777');
+        setCardColor('#ef5777');
         break;
       case IN_PROGRESS:
-        setBadgeColor('#B33771');
+        setCardColor('#B33771');
         break;
       case DONE:
-        setBadgeColor('#341f97');
+        setCardColor('#341f97');
         break;
       case NOTE:
-        setBadgeColor('#130f40');
+        setCardColor('#130f40');
         break;
     }
   }, [item]);
@@ -106,12 +106,15 @@ function Card({ item }) {
 
   return (
     <div
-      className="cardWrap"
+      className={styles.cardWrap}
       ref={dragRef}
-      style={{ opacity: isDragging ? '0.3' : '1' }}
+      style={{
+        opacity: isDragging ? '0.3' : '1',
+       }}
       onClick={handleClick}
     >
-      <div className="cardHeaderWrap">
+      <span></span>
+      <div className={styles.cardHeaderWrap}>
         <h5>{item.title}</h5>
         <p>{item.content}</p>
       </div>
