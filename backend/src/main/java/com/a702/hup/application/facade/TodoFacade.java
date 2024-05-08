a1e2fb8ea75dcdce2cf31d35206bd60585a65106
BoyCho <iijokv@naver.com>
2024-05-08 12:52:07 +0900

    feat : 할 일 생성자 검증

--- backend/src/main/java/com/a702/hup/application/facade/TodoFacade.java
+++ backend/src/main/java/com/a702/hup/application/facade/TodoFacade.java
@@ -47,9 +47,19 @@ public class TodoFacade {
     @Transactional
     public void saveAssignee(int memberId, TodoAssigneeSaveRequest request) {
         Todo todo = todoService.findById(request.getTodoId());
+        validation(todo, memberId);
 
         Member assignee = memberService.findById(request.getMemberId());
         todoMemberService.save(todo, assignee);
     }
 
+    /**
+     * @author 손현조
+     * @date 2024-05-07
+     * @description 해당 이슈에 권한이 있는지 검사
+     */
+    private void validation(Todo todo, int memberId){
+        Member member = memberService.findById(memberId);
+        issueMemberService.validationRole(todo.getIssue(), member);
+    }
 }
