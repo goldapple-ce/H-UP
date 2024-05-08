0f6762a2063d42d1744f552b852a623c650352c0
김한중 <hjhzkc@gmail.com>
2024-05-08 16:49:06 +0900

    fix: interceptor에서 useSelector 제거

--- frontend/src/api/interceptor/interceptor.jsx
+++ frontend/src/api/interceptor/interceptor.jsx
@@ -6,16 +6,15 @@ export default function interceptor(api) {
     addTokenOnRequest(api)
 }
 
-function addTokenOnRequest(instance) {
+function addTokenOnRequest(instance, token) {
     instance.interceptors.request.use(
     config => {
-        const token = useSelector(state => state.auth.token);
 
         if (token) {
             config.headers.Authorization = `Bearer ${token}`
         }
 
-        return config
+        return config;
     },
     error => Promise.reject(error.response),
     )
