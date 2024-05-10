bd242dc11233af12efc69c139fdc10521c89de85
lkt9899 <lkt9899@naver.com>
2024-05-10 13:20:56 +0900

    fix : 새로고침 시 토큰 사라지는 문제 해결

--- frontend/src/recoil/auth.jsx
+++ frontend/src/recoil/auth.jsx
@@ -3,7 +3,7 @@ import { atom } from 'recoil';
 const authState = atom({
   key: 'authState',
   default: {
-    isLogin: false,
+    isLogin: sessionStorage.getItem('accessToken') ? true : false,
     memberId: '',
   },
 });
