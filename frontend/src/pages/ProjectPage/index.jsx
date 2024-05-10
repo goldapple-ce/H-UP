1f34b6d5c00fcf1cce05418c4a598abe46fff663
bluebear9508@naver.com <bluebear9508@naver.com>
2024-05-10 13:36:21 +0900

    feat: Making Issue Create Button

--- frontend/src/pages/ProjectPage/index.jsx
+++ frontend/src/pages/ProjectPage/index.jsx
@@ -1,10 +1,27 @@
 import MainTab from '@component/MainTab/MainTab';
 import styles from './ProjectPage.module.scss'; // SCSS 스타일 시트 임포트
+import IssueAddButton from '@component/IssueAddButton/IssueAddButton';
+import { createIssue } from '@api/services/issue';
+import { useParams } from 'react-router-dom';
+
+async function makingIssueId(projectId) {
+  const data = {
+    projectId: projectId
+  };
+
+  const response = await createIssue(data);
+}
 
 const ProjectPage = props => {
+  
+  const { id } = useParams();
+
   return (
     <div className={styles.issue_container}>
       <h2>프로젝트 페이지</h2>
+      <div>
+        <IssueAddButton text={'이슈추가'} onClick={() => makingIssueId(id)}/>
+      </div>
       <div>
         <MainTab />
       </div>
