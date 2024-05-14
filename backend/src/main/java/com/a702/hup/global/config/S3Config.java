a9beee78c29d6df2a6798182603242b4c3eb6176
lkt9899 <lkt9899@naver.com>
2024-05-14 10:42:53 +0900

    feat : S3 연동 - S3 연동 완료 - 도메인 별 파일 업로드 기능 구현 - S10P31A702-258

---
+++ backend/src/main/java/com/a702/hup/global/config/S3Config.java
@@ -0,0 +1,48 @@
+package com.a702.hup.global.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Slf4j
+@Configuration
+public class S3Config {
+
+    @Value("${spring.cloud.aws.credentials.access-key}")
+    private String accessKey;
+    @Value("${spring.cloud.aws.credentials.secret-key}")
+    private String secretKey;
+    @Value("${spring.cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    public S3Client s3Client() {
+        log.info("[+] S3Config :: start");
+        log.info("[+] S3Config :: region : {}", Region.of(region));
+        log.info("[+] S3Config :: accessKey : {}", accessKey);
+        log.info("[+] S3Config :: secretKey : {}", secretKey);
+        log.info("[+] S3Config :: end");
+        return S3Client.builder()
+                .credentialsProvider(this::awsCredentials)
+                .region(Region.of(region))
+                .build();
+    }
+
+    private AwsCredentials awsCredentials() {
+        return new AwsCredentials() {
+            @Override
+            public String accessKeyId() {
+                return accessKey;
+            }
+
+            @Override
+            public String secretAccessKey() {
+                return secretKey;
+            }
+        };
+    }
+}
