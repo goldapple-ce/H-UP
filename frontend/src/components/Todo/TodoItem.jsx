cc11dcc584acc0df42400457c25c839ee1e50aa2
bluebear9508@naver.com <bluebear9508@naver.com>
2024-05-16 14:49:08 +0900

    Feat: Connecting Todo API

--- frontend/src/components/Todo/TodoItem.jsx
+++ frontend/src/components/Todo/TodoItem.jsx
@@ -7,30 +7,30 @@ const TodoItem = ({ Todo }) => {
   const { todoId, content, createdAt, endAt, requester, assigneeList } = Todo;
   const [iconImage, setIconImage] = useState('');
 
-  const formatToDate = jsDateStr => {
-    const date = new Date(jsDateStr);
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
-    // const hour = date.getHours();
-    // const minutes = date.getMinutes();
-    // const formattedDate = `${year}년 ${month}월 ${day}일 ${hour}:${minutes}`;
-    const formattedDate = `${year}. ${month}. ${day}`;
-    return formattedDate;
-  };
+  // const formatToDate = jsDateStr => {
+  //   const date = new Date(jsDateStr);
+  //   const year = date.getFullYear();
+  //   const month = date.getMonth() + 1;
+  //   const day = date.getDate();
+  //   // const hour = date.getHours();
+  //   // const minutes = date.getMinutes();
+  //   // const formattedDate = `${year}년 ${month}월 ${day}일 ${hour}:${minutes}`;
+  //   const formattedDate = `${year}. ${month}. ${day}`;
+  //   return formattedDate;
+  // };
 
   return (
     <div className={styles.Todo}>
       <img src={playIcon} alt='' />
       <h5>{content}</h5>
       <ul>
-        <p>{formatToDate(createdAt)} ~ {formatToDate(endAt)}</p>
+        {/* <p>{formatToDate(createdAt)} ~ {formatToDate(endAt)}</p> */}
       </ul>
       {assigneeList.map(assignee => (
         <img
           className={styles.Todo__assignee}
           key={assignee.id}
-          src={assignee.img}
+          src={assignee.img ? assignee.img : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTBMWAdByZhgGVn9XczgDSm6qIfh6c5HaXYrFXGGcJTcOZRK4Qj9A09gE1cw&s'}
           alt={assignee.name}
         />
       ))}
