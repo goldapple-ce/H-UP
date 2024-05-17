9333184000bd0f6fb0578186493f821bfbb19a58
lkt9899 <lkt9899@naver.com>
2024-05-17 14:28:02 +0900

    fix : teamId persist

--- frontend/src/recoil/info.jsx
+++ frontend/src/recoil/info.jsx
@@ -1,4 +1,7 @@
 import { atom } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
+
+const { persistAtom } = recoilPersist();
 
 export const infoState = atom({
   key: 'infoState',
@@ -6,4 +9,5 @@ export const infoState = atom({
     teamId: 0,
     projectId: 0,
   },
+  effects_UNSTABLE: [persistAtom],
 });
