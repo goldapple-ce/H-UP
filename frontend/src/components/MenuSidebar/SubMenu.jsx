c6d265ab3a5886df7c687880d152be9cec21529a
김한중 <hjhzkc@gmail.com>
2024-05-17 16:00:05 +0900

    fix: toLowerCase null 수정

--- frontend/src/components/MenuSidebar/SubMenu.jsx
+++ frontend/src/components/MenuSidebar/SubMenu.jsx
@@ -37,7 +37,7 @@ const SubMenu = ({ item }) => {
       >
         <div>
           <div>
-            <subMenuSpan>{item.name}</subMenuSpan>
+            <span>{item.name}</span>
           </div>
           <div>
             {issueList && subnav
