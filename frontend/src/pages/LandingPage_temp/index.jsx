921753b70fb89db94dca15604b7e1bbffa31f8a9
goldapple-ce <84130518+goldapple-ce@users.noreply.github.com>
2024-05-18 19:35:11 +0900

    Fix : Floating버튼 위치 변경 및 프로젝트에서만 가능하게

--- frontend/src/pages/LandingPage_temp/index.jsx
+++ frontend/src/pages/LandingPage_temp/index.jsx
@@ -1,11 +1,8 @@
-import FloatingButton from '@component/FloatingButton/FloatingButton';
-import styles from './LandingPage.module.scss'; // SCSS 스타일 시트 임포트
-
 
 const LandingPage = props => {
   return (
     <div>
-      <FloatingButton />
+      반가워요!
     </div>
   );
 };
