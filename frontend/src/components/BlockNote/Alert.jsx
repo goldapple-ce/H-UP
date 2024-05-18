7220bebdfa68b0859471f09a375262852448c609
bluebear9508@naver.com <bluebear9508@naver.com>
2024-05-19 02:00:19 +0900

    Feat: implemented Agenda and Todo on IssuePage

--- frontend/src/components/BlockNote/Alert.jsx
+++ frontend/src/components/BlockNote/Alert.jsx
@@ -1,9 +1,10 @@
 import { defaultProps } from "@blocknote/core";
-import { createReactBlockSpec } from "@blocknote/react";
+import { createReactBlockSpec, createReactInlineContentSpec } from "@blocknote/react";
 import { Menu } from "@mantine/core";
 import { MdCheckCircle, MdError, MdPlayCircle } from "react-icons/md";
 import "./ToggleType.css";
- 
+import { CaretRightFill } from 'react-bootstrap-icons';
+
 // The types of alerts that users can choose from.
 export const Toggles = [
   {
@@ -47,7 +48,26 @@ export const Toggles = [
     },
   },
 ];
- 
+
+export const Mention = createReactInlineContentSpec(
+  {
+    type: "mention",
+    propSchema: {
+      userName: {
+        default: "",
+      },
+    },
+    content: "inline",
+  },
+  {
+    render: (props) => (
+      <span style={{ backgroundColor: "#8400ff33" }}>
+        @{props.inlineContent.props.userName}
+      </span>
+    ),
+  }
+);
+
 // The Alert block.
 export const Alert = createReactBlockSpec(
   {
@@ -59,6 +79,12 @@ export const Alert = createReactBlockSpec(
         default: "ASSIGNED",
         values: ["ASSIGNED", "COMPLETED", "COMPLETED", "APPROVED"],
       },
+      mention: {
+        default: '멘션',
+      },
+      img: {
+        default: 'https://mblogthumb-phinf.pstatic.net/MjAyMDExMDFfMTgy/MDAxNjA0MjI4ODc1NDMw.Ex906Mv9nnPEZGCh4SREknadZvzMO8LyDzGOHMKPdwAg.ZAmE6pU5lhEdeOUsPdxg8-gOuZrq_ipJ5VhqaViubI4g.JPEG.gambasg/%EC%9C%A0%ED%8A%9C%EB%B8%8C_%EA%B8%B0%EB%B3%B8%ED%94%84%EB%A1%9C%ED%95%84_%ED%95%98%EB%8A%98%EC%83%89.jpg?type=w800'
+      }
     },
     content: "inline",
   },
@@ -111,6 +137,104 @@ export const Alert = createReactBlockSpec(
           </Menu>
           {/*Rich text field for user to type in*/}
           <div className={"inline-content"} ref={props.contentRef} />
+          <img styles="width:30px; height:30px; border-radius:50%" src={props.block.props.img}/>
+          <div className="name">
+          {props.block.props.mention}
+          </div>
+          
+          
+        </div>
+      );
+    },
+  }
+);
+
+// The Alert block.
+export const Agenda = createReactBlockSpec(
+  {
+    type: "agenda",
+    propSchema: {
+      textAlignment: defaultProps.textAlignment,
+      textColor: defaultProps.textColor,
+      type: {
+        default: "ASSIGNED",
+        values: ["ASSIGNED", "COMPLETED", "COMPLETED", "APPROVED"],
+      },
+      mention: {
+        default: '멘션',
+      },
+      assignee: {
+        default: '수신자',
+      },
+      assigneeimg: {
+        default: 'https://mblogthumb-phinf.pstatic.net/MjAyMDExMDFfMTgy/MDAxNjA0MjI4ODc1NDMw.Ex906Mv9nnPEZGCh4SREknadZvzMO8LyDzGOHMKPdwAg.ZAmE6pU5lhEdeOUsPdxg8-gOuZrq_ipJ5VhqaViubI4g.JPEG.gambasg/%EC%9C%A0%ED%8A%9C%EB%B8%8C_%EA%B8%B0%EB%B3%B8%ED%94%84%EB%A1%9C%ED%95%84_%ED%95%98%EB%8A%98%EC%83%89.jpg?type=w800'
+      },
+      mentionimg: {
+        default: 'https://mblogthumb-phinf.pstatic.net/MjAyMDExMDFfMTgy/MDAxNjA0MjI4ODc1NDMw.Ex906Mv9nnPEZGCh4SREknadZvzMO8LyDzGOHMKPdwAg.ZAmE6pU5lhEdeOUsPdxg8-gOuZrq_ipJ5VhqaViubI4g.JPEG.gambasg/%EC%9C%A0%ED%8A%9C%EB%B8%8C_%EA%B8%B0%EB%B3%B8%ED%94%84%EB%A1%9C%ED%95%84_%ED%95%98%EB%8A%98%EC%83%89.jpg?type=w800'
+      }
+    },
+    content: "inline",
+  },
+  {
+    render: (props) => {
+      const Toggle = Toggles.find(
+        (a) => a.value === props.block.props.type
+      );
+      const Icon = Toggle.icon;
+      return (
+        <div className={"alert"} data-alert-type={props.block.props.type}>
+          {/*Icon which opens a menu to choose the Alert type*/}
+          <Menu withinPortal={false} zIndex={999999}>
+            <Menu.Target>
+              <div className={"alert-icon-wrapper"} contentEditable={false}>
+                <Icon
+                  className={"alert-icon"}
+                  data-alert-icon-type={props.block.props.type}
+                  size={32}
+                />
+              </div>
+            </Menu.Target>
+            {/*Dropdown to change the Alert type*/}
+            <Menu.Dropdown>
+              <Menu.Label>Toggle Type</Menu.Label>
+              <Menu.Divider />
+              {Toggles.map((type) => {
+                const ItemIcon = type.icon;
+ 
+                return (
+                  <Menu.Item
+                    key={type.value}
+                    leftSection={
+                      <ItemIcon
+                        className={"alert-icon"}
+                        data-alert-icon-type={type.value}
+                      />
+                    }
+                    onClick={() =>
+                      props.editor.updateBlock(props.block, {
+                        type: "alert",
+                        props: { type: type.value },
+                      })
+                    }>
+                    {type.title}
+                  </Menu.Item>
+                );
+              })}
+            </Menu.Dropdown>
+          </Menu>
+          {/*Rich text field for user to type in*/}
+          <div className={"inline-content"} ref={props.contentRef} />
+          <img styles="width:30px; height:30px; border-radius:50%" src={props.block.props.mentionimg}/>
+          <div className="name">
+          {props.block.props.mention}
+          </div>
+          <CaretRightFill className="caret" />
+          <img styles="width:30px; height:30px; border-radius:50%" src={props.block.props.assigneeimg}/>
+          <div className="name">
+          {props.block.props.assignee}
+          </div>
+          
+          
         </div>
       );
     },
