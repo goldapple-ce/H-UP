95340ab63ba98bcb62a9df80501f3b04008ae024
서울_18반_김은비 <dnpfkdlvm123@naver.com>
2024-05-18 21:59:56 +0900

    feat: 플로팅버튼 이슈추가 수정

--- frontend/src/pages/ProjectPage/index.jsx
+++ frontend/src/pages/ProjectPage/index.jsx
@@ -1,62 +1,17 @@
 import MainTab from '@component/MainTab/MainTab';
-import styles from './ProjectPage.module.scss'; // SCSS 스타일 시트 임포트
-import { useParams } from 'react-router-dom';
-import { createIssue, updateIssue } from '@api/services/issue';
-import IssueAddButton from '@component/IssueAddButton/IssueAddButton';
-import Modal from 'react-modal';
-import { useState } from 'react';
 import FloatingButton from '@component/FloatingButton/FloatingButton';
+import styles from './ProjectPage.module.scss'; // SCSS 스타일 시트 임포트
 
 export default function ProjectPage() {
-  const { id } = useParams();
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [title, setTitle] = useState('');
-  const [startDate, setStartDate] = useState('');
-  const [endDate, setEndDate] = useState('');
-
-  const openModal = () => {
-    setModalIsOpen(true);
-  };
-
-  const closeModal = () => {
-    setModalIsOpen(false);
-    setStartDate('');
-    setEndDate('');
-    setTitle('');
-  };
-
-  const handleSubmit = async e => {
-    e.preventDefault();
-    makeIssue();
-    closeModal();
-  };
-
-  const makeIssue = async () => {
-    const response = await createIssue({ projectId: id });
-    const issueId = response.data.issueId;
-
-    const newIssue = {
-      issueId: issueId,
-      title: title,
-      startDate: startDate,
-      endDate: endDate,
-      status: 'CREATED',
-    };
-
-    await updateIssue(newIssue);
-  };
 
   return (
     <div className={styles.issue_container}>
       <h2>프로젝트 페이지</h2>
       <div>
-        <IssueAddButton text={'이슈추가'} onClick={setModalIsOpen} />
-      </div>
-      <div>
         <MainTab />
       </div>
       <FloatingButton/>
-      <Modal
+      {/* <Modal
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
         className={styles.modal}
@@ -108,7 +63,10 @@ export default function ProjectPage() {
             </button>
           </div>
         </form>
-      </Modal>
+      </Modal> */}
+      <div>
+      <FloatingButton />
+      </div>
     </div>
   );
 }
