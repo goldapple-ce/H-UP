a5657644fb9f892d78bb586687d874b78ca08d35
lkt9899 <lkt9899@naver.com>
2024-05-20 02:01:26 +0900

    fix : css 오류 수정

--- frontend/src/components/common/UserIcon.jsx
+++ frontend/src/components/common/UserIcon.jsx
@@ -1,6 +1,8 @@
 import noImage from '@asset/img/no_image.png';
-import './UserIcon.module.scss';
+import styles from './UserIcon.module.scss';
 
 export default function UserIcon({ src, alt }) {
-  return <img src={src ? src : noImage} alt={alt} />;
+  return (
+    <img className={styles.user_icon} src={src ? src : noImage} alt={alt} />
+  );
 }
