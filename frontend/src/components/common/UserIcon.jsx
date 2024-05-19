83f9dca0fd1ffeacfa0a6c252c4d277c77e63055
lkt9899 <lkt9899@naver.com>
2024-05-20 01:38:59 +0900

    fix : css 미적용 오류 수정

--- frontend/src/components/common/UserIcon.jsx
+++ frontend/src/components/common/UserIcon.jsx
@@ -1,4 +1,5 @@
 import noImage from '@asset/img/no_image.png';
+import './UserIcon.module.scss';
 
 export default function UserIcon({ src, alt }) {
   return <img src={src ? src : noImage} alt={alt} />;
